8f965e404d338236d669d427367381a3c997136a
kazuki matsumoto <karasusan@gmail.com>
2018-07-01 16:15:20 +0900

    change package arguments

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="zogi", channel="stable")
+    builder = ConanMultiPackager(username="kazuki", channel="stable")
     builder.add_common_builds(shared_option_name="blosc:shared")
     builder.run()
