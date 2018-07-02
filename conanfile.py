b3841a648e8035b3743b970a9d1a13c58767321a
kazuki matsumoto <karasusan@gmail.com>
2018-07-02 15:23:02 +0900

    remove default_options "shared=True"

--- conanfile.py
+++ conanfile.py
@@ -13,7 +13,7 @@ class BloscConan(ConanFile):
     options = { "shared": [True, False]
               , "fPIC": [True, False]
               }
-    default_options = "shared=True", "fPIC=True"
+    default_options = "fPIC=True"
     exports = ["FindBlosc.cmake", "fix-shared-lib-install.patch"]
 
     def config_options(self):
